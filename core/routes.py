3b250ff84ded18c3fba772ec0208c88e93e9607b
mhsiddiqui <mhassan.eeng@gmail.com>
2024-10-22 23:15:22 +0500

    FastAPI Boilerplate

--- core/routes.py
+++ core/routes.py
@@ -3,5 +3,4 @@ from core.easy_router.route import Route, include
 routes = [
     Route(path="/api/auth/v1", endpoint=include("src.features.auth.routes"), tags=["Authentication"]),
     Route(path="/api/account/v1", endpoint=include("src.features.account.routes"), tags=["Account"]),
-    Route(path="/api/otp/v1", endpoint=include("src.features.otp.routes"), tags=["OTP"]),
 ]
